2bc6b3e15d318a986cf3bc824f73267797d805c5
Charlie Lee <clcblee@gmail.com>
2018-03-07 14:44:31 -0800

    Run npm install

---
+++ node_modules/testarmada-manifest/test/data/complete-data-m8.js
@@ -0,0 +1,249 @@
+module.exports = [
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event",
+      "data": {
+        "name": "magellan-run",
+        "markers": [
+          {
+            "name": "start",
+            "t": 1477673495973
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event",
+      "data": {
+        "name": "magellan-busy",
+        "markers": [
+          {
+            "name": "idle",
+            "t": 1477673495973
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event",
+      "data": {
+        "name": "acquire-worker-202b372b796380",
+        "markers": [
+          {
+            "name": "start",
+            "t": 1477673496116
+          }
+        ]
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event-mark",
+      "eventName": "acquire-worker-202b372b796380",
+      "data": {
+        "name": "end",
+        "t": 1477673497137
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event-mark",
+      "eventName": "magellan-busy",
+      "data": {
+        "name": "busy",
+        "t": 1477673498150
+      }
+    }
+  },
+  {
+    "type": "testRun",
+    "testRun": {
+      "guid": "155c2a55771841",
+      "buildId": "magellan-516b4a60d8909",
+      "tempAssetPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets",
+      "environmentId": "chrome",
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "seleniumPort": 12001,
+      "mockingPort": 12000,
+      "configPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets/nightwatch.json"
+    },
+    "test": {
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "maxAttempts": 3,
+      "attempts": 0,
+      "status": 1,
+      "browser": {
+        "browserId": "chrome"
+      },
+      "workerIndex": -1,
+      "stdout": "",
+      "stderr": ""
+    },
+    "message": {
+      "type": "worker-status",
+      "status": "started",
+      "name": "test/tests//simple.js"
+    }
+  },
+  {
+    "type": "testRun",
+    "testRun": {
+      "guid": "155c2a55771841",
+      "buildId": "magellan-516b4a60d8909",
+      "tempAssetPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets",
+      "environmentId": "chrome",
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "seleniumPort": 12001,
+      "mockingPort": 12000,
+      "configPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets/nightwatch.json"
+    },
+    "test": {
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "maxAttempts": 3,
+      "attempts": 0,
+      "status": 1,
+      "browser": {
+        "browserId": "chrome"
+      },
+      "workerIndex": -1,
+      "stdout": "",
+      "stderr": ""
+    },
+    "message": {
+      "type": "analytics-event",
+      "data": {
+        "name": "test-run-155c2a55771841",
+        "markers": [
+          {
+            "name": "start",
+            "t": 1477673498152
+          }
+        ],
+        "metadata": {
+          "test": "test/tests//simple.js",
+          "browser": "chrome",
+          "attemptNumber": 1
+        }
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event-mark",
+      "eventName": "magellan-busy",
+      "data": {
+        "name": "idle",
+        "t": 1477673507774
+      }
+    }
+  },
+  {
+    "type": "testRun",
+    "testRun": {
+      "guid": "155c2a55771841",
+      "buildId": "magellan-516b4a60d8909",
+      "tempAssetPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets",
+      "environmentId": "chrome",
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "seleniumPort": 12001,
+      "mockingPort": 12000,
+      "configPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets/nightwatch.json"
+    },
+    "test": {
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "maxAttempts": 3,
+      "attempts": 0,
+      "status": 1,
+      "browser": {
+        "browserId": "chrome"
+      },
+      "workerIndex": -1,
+      "stdout": "",
+      "stderr": "",
+      "startTime": 1477673498153
+    },
+    "message": {
+      "type": "analytics-event-mark",
+      "eventName": "test-run-155c2a55771841",
+      "data": {
+        "name": "passed",
+        "t": 1477673507775
+      }
+    }
+  },
+  {
+    "type": "testRun",
+    "testRun": {
+      "guid": "155c2a55771841",
+      "buildId": "magellan-516b4a60d8909",
+      "tempAssetPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets",
+      "environmentId": "chrome",
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "seleniumPort": 12001,
+      "mockingPort": 12000,
+      "configPath": "/Users/jherri1/magellan/sandbox/temp/build-magellan-516b4a60d8909_155c2a55771841__temp_assets/nightwatch.json"
+    },
+    "test": {
+      "locator": {
+        "filename": "test/tests//simple.js"
+      },
+      "maxAttempts": 3,
+      "attempts": 0,
+      "status": 1,
+      "browser": {
+        "browserId": "chrome"
+      },
+      "workerIndex": -1,
+      "stdout": "",
+      "stderr": "",
+      "runningTime": 9622,
+      "startTime": 1477673498153
+    },
+    "message": {
+      "type": "worker-status",
+      "status": "finished",
+      "name": "test/tests//simple.js",
+      "passed": true,
+      "metadata": {
+        "resultURL": "https://saucelabs.com/tests/31719756-210e-446b-bd23-cb88c5120147"
+      }
+    }
+  },
+  {
+    "type": "global",
+    "message": {
+      "type": "analytics-event-mark",
+      "eventName": "magellan-run",
+      "data": {
+        "name": "passed",
+        "t": 1477673510283
+      }
+    }
+  }
+];
